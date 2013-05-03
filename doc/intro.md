14181817ee4c471f138116ffc29926b5eb165612
Ghadi Shayban <gshayban@gmail.com>
2013-05-02 21:56:13 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to qcouple
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject qcouple "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[com.lmax/disruptor "3.0.1"]
+                 [org.clojure/clojure "1.5.1"]])
